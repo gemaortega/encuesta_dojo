9dc0b7b4ab855c8d1e8638c95a3f88734fbf6a29
Gema Ortega <gemaortegaparra@gmail.com>
2022-10-03 16:11:10 -0300

    primer commit

---
+++ flask_app/controllers/surveys.py
@@ -0,0 +1,24 @@
+from flask import render_template, redirect, request, session
+from flask_app import app
+from flask import Flask
+
+@app.route('/')
+def index():
+    return render_template('survey.html')
+
+@app.route('/process', methods=["POST"])
+def checkout():
+    print(request.form)
+
+    session['name'] = request.form ['name']
+    session['city'] = request.form ['city']
+    session['language'] = request.form ['language']
+    session['comment'] = request.form ['comment']
+    
+    return redirect('/results')
+
+    @app.route('/results')
+    def data():
+        print(session)
+        return render_template( 'results.html', name=session['name'], city=session['city'], languaje=session['languaje'], comments=session['comments'])
+
